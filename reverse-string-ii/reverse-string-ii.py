acce5f393f2bc28b9ebb7c9eae6e9e5daad31e20
Chethana Saubhagya Wickramasinghe <chewick@my.yorku.ca>
2021-06-02 01:31:26 -0400

    Time: 52 ms (7.37%), Space: 14.5 MB (41.31%) - LeetHub

---
+++ reverse-string-ii/reverse-string-ii.py
@@ -0,0 +1,42 @@
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+    # example solution
+        a = list(s)
+        for i in range(0, len(a), 2*k):
+            a[i:i+k] = reversed(a[i:i+k])
+        return "".join(a)
+    
+    # my code breaks at once cases, passes all given and basic cases
+#         new = ""
+#         length_s = len(s)
+#         count = 0
+#         start_k = k
+        
+#         if k > length_s:
+#             return s[::-1]
+        
+#         if k ==1:
+#             return s
+        
+#         for l in s:
+#             count += 1
+            
+#             if count == k or (count == length_s and count < k ):
+#                 reverse = s[k-start_k:k][::-1]
+#                 new = new[:-start_k+1]
+#                 new += reverse
+#                 k = k + 2*k
+#             else:
+#                 new += l
+                
+#         return new
+        
+            
+        
+            
+            
+            
+            
+            
+            
+# abc , k =2 -> cba

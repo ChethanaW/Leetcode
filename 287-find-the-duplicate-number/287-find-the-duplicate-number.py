1461083e2fc532cc07a0c1e3a0d92ccfb97fd605
Chethana Saubhagya Wickramasinghe <chewick@my.yorku.ca>
2022-01-10 17:54:52 -0500

    Time: 1021 ms (15.95%), Space: 31.9 MB (9.36%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -1,6 +1,16 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-
+                
+#         timed out bcs worst case run time is o(n^2)
+#         count = 0
+#         for e in nums:
+#             if e in nums[count+1:]:
+#                 return e
+            
+#             count = count + 1
+        
+    
+    #much faster since worst case is run time 0(n) times
         dictionary = {}
         
         for e in nums:
